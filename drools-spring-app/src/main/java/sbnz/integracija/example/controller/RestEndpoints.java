c7ebb282237fa9f3371f8d61b25ec49626f84652
nikolaroncevic <nikola.roncevic123@gmail.com>
2020-05-13 19:03:32 +0200

    manji commit

---
+++ drools-spring-app/src/main/java/sbnz/integracija/example/controller/RestEndpoints.java
@@ -0,0 +1,26 @@
+package sbnz.integracija.example.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import sbnz.integracija.example.facts.Item;
+
+@RestController(value = "/api")
+public class RestEndpoints {
+	
+	@RequestMapping(value = "/item", method = RequestMethod.GET, produces = "application/json")
+	public Item getQuestions(@RequestParam(required = true) String id, @RequestParam(required = true) String name,
+			@RequestParam(required = true) double cost, @RequestParam(required = true) double salePrice) {
+
+		Item newItem = new Item(Long.parseLong(id), name, cost, salePrice);
+
+		log.debug("Item request received for: " + newItem);
+
+		Item i2 = sampleService.getClassifiedItem(newItem);
+
+		return i2;
+	}
+
+}
